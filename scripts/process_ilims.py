636553a37ac06cc177388f1f089c1bca7de864c3
ShwetaReddy25 <sweta.reddy@1cell.ai>
2026-01-27 13:47:42 +0530

    Update process_ilims.py

--- scripts/process_ilims.py
+++ scripts/process_ilims.py
@@ -50,7 +50,7 @@ asm_df = pd.read_excel("data/email grouping updated.xlsx", engine="openpyxl")
 asm_df.columns = asm_df.columns.astype(str).str.strip()
 
 # AUTO FIND EMAIL COLUMN
-possible_email_cols = [c for c in asm_df.columns if "email" in c.lower() and "id" in c.lower()]
+possible_email_cols = [c for c in asm_df.columns if "Email - Id"]
 if len(possible_email_cols) == 0:
     raise Exception("Email column not found in ASM Excel file. Please check column names.")
 
